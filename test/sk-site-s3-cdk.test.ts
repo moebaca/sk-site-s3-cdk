7974bf1291c808ac83de96d994b11853952cb66f
Hopkins <mathopk@3c0630178163.ant.amazon.com>
2022-03-18 16:15:54 -0700

    Initial commit

---
+++ test/sk-site-s3-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as SkSiteS3Cdk from '../lib/sk-site-s3-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/sk-site-s3-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new SkSiteS3Cdk.SkSiteS3CdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
