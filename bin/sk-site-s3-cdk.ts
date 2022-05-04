13d3aaef749598d229d7159d1edf892d691a9a02
Matt Hopkins <moebaca@hotmail.com>
2022-05-04 16:05:06 -0700

    Updated paramaters on sk-site-s3-cdk class

--- bin/sk-site-s3-cdk.ts
+++ bin/sk-site-s3-cdk.ts
@@ -14,35 +14,35 @@ import { SkSiteS3CdkStack } from '../lib/sk-site-s3-cdk-stack';
  * }
 **/
 class SKSiteStack extends cdk.Stack {
-    constructor(parent: cdk.App, name: string, props: cdk.StackProps) {
-      super(parent, name, props);
+  constructor(parent: cdk.App, name: string, props: cdk.StackProps) {
+    super(parent, name, props);
 
-      new SkSiteS3CdkStack(this, 'Site', {
-        domainName: this.node.tryGetContext('domain'),
-        emailAddr: this.node.tryGetContext('emailAddr'),
-        captchaSecret: this.node.tryGetContext('captchaSecret')
-      });
-    }
+    new SkSiteS3CdkStack(this, 'Site', {
+      domainName: this.node.tryGetContext('domain'),
+      emailAddr: this.node.tryGetContext('emailAddr'),
+      captchaSecret: this.node.tryGetContext('captchaSecret')
+    });
+  }
 }
 
 const app = new cdk.App();
 
 new SKSiteStack(app, 'SKSite', {
+  /**
+   * This is required for our use of hosted-zone lookup.
+   *
+   * Lookups do not work at all without an explicit environment
+   * specified; to use them, you must specify env.
+   * @see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
+   */
+  env: {
+    account: app.node.tryGetContext('accountId'),
     /**
-     * This is required for our use of hosted-zone lookup.
-     *
-     * Lookups do not work at all without an explicit environment
-     * specified; to use them, you must specify env.
-     * @see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
+     * Stack must be in us-east-1, because the ACM certificate for a
+     * global CloudFront distribution must be requested in us-east-1.
      */
-    env: {
-        account: app.node.tryGetContext('accountId'),
-        /**
-         * Stack must be in us-east-1, because the ACM certificate for a
-         * global CloudFront distribution must be requested in us-east-1.
-         */
-        region: 'us-east-1',
-    }
+    region: 'us-east-1',
+  }
 });
 
 app.synth();
